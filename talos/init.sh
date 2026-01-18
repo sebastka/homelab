0758192b9ae2a8da3e4882a0825d93b2459332a8
Sebastian Karlsen <sebastian@karlsen.fr>
2026-01-18 11:05:38 +0100

    Refactor

--- talos/init.sh
+++ talos/init.sh
@@ -10,28 +10,37 @@ main()
 
     printf 'Generating Talos configuration...\n'
     rm -rf "$TALOS_CONFIG_HOME"
-    talosctl gen config "$CLUSTER_NAME" "https://$CONTROL_PLANE_IP:6443" --output-dir "$TALOS_CONFIG_HOME" --install-image "$FACTORY_IMAGE"\
-        --config-patch @patch/k8s/gatewayapi.yaml \
-        --config-patch @patch/cilium/cilium.yaml \
-        --config-patch @patch/talos/diskSelector.yaml \
-        --config-patch @patch/longhorn/requirements.yaml
+    talosctl gen config "$CLUSTER_NAME" "https://$CONTROL_PLANE_IP:6443" --output-dir "$TALOS_CONFIG_HOME" --install-image "$FACTORY_IMAGE" \
+        --config-patch @patch/common/sysctl.yaml \
+        --config-patch @patch/common/gatewayapi.yaml \
+        --config-patch @patch/common/cni-proxy.yaml \
+        --config-patch @patch/common/diskSelector.yaml \
+        --config-patch @patch/common/longhorn.yaml
 
     ln -sf "$CLUSTER_NAME/talosconfig" "$XDG_CONFIG_HOME/talos/config.yaml"
 
     wait 'Press enter to apply configuration to control plane nodes...'
-    for CP_IP in $TALOS_CPS; do
-        talosctl apply-config --insecure --nodes "$CP_IP" --file "$TALOS_CONFIG_HOME/controlplane.yaml" \
-            --config-patch @patch/talos/vip-machine.yaml
+    sed 1d nodes.csv | grep controlplane | while IFS=, read cluster pve_node role node_name ip; do
+        yq -n ".machine.nodeLabels.\"topology.kubernetes.io/region\" = \"$cluster\" | .machine.nodeLabels.\"topology.kubernetes.io/zone\" = \"$node_name\"" \
+            > /tmp/labels.yaml
+
+        talosctl apply-config --insecure --nodes "$ip" --file "$TALOS_CONFIG_HOME/controlplane.yaml" \
+            --config-patch @patch/cp/vip.yaml \
+            --config-patch @/tmp/labels.yaml
     done
 
     talosctl config endpoint $CONTROL_PLANE_IP
     talosctl config node $CONTROL_PLANE_IP
 
     wait 'Press enter to apply configuration to worker nodes...'
-    for WORKER_IP in $TALOS_WORKERS; do
-        talosctl apply-config --insecure --nodes "$WORKER_IP" --file "$TALOS_CONFIG_HOME/worker.yaml" \
-            --config-patch @patch/talos/vip-cluster.yaml \
-            --config-patch @patch/longhorn/disk.yaml --config-patch @patch/longhorn/mount.yaml
+    sed 1d nodes.csv | grep worker | while IFS=, read cluster pve_node role node_name ip; do
+        yq -n ".machine.nodeLabels.\"topology.kubernetes.io/region\" = \"$cluster\" | .machine.nodeLabels.\"topology.kubernetes.io/zone\" = \"$node_name\"" \
+            > /tmp/labels.yaml
+
+        talosctl apply-config --insecure --nodes "$ip" --file "$TALOS_CONFIG_HOME/worker.yaml" \
+            --config-patch @patch/worker/vip.yaml \
+            --config-patch @patch/worker/longhorn.yaml \
+            --config-patch @/tmp/labels.yaml
     done
 
     wait 'Press enter to bootstrap the cluster...'
