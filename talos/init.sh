1f6b21402933b5c0bc3b672b0c00c64d9b3c3cf0
Sebastian Karlsen <sebastian@karlsen.fr>
2026-02-05 09:23:06 +0100

    Install metrics-server

--- talos/init.sh
+++ talos/init.sh
@@ -13,7 +13,8 @@ main()
     talosctl gen config "$CLUSTER_NAME" "https://$CONTROL_PLANE_IP:6443" --output-dir "$TALOS_CONFIG_HOME" --install-image "$FACTORY_IMAGE" \
         --config-patch @patch/common/diskSelector.yaml \
         --config-patch @patch/common/longhorn.yaml \
-        --config-patch @patch/common/cni-proxy.yaml
+        --config-patch @patch/common/cni-proxy.yaml \
+        --config-patch @patch/common/metrics-server.yaml
 
     ln -sf "$CLUSTER_NAME/talosconfig" "$XDG_CONFIG_HOME/talos/config.yaml"
 
@@ -24,6 +25,7 @@ main()
 
         talosctl apply-config --insecure --nodes "$ip" --file "$TALOS_CONFIG_HOME/controlplane.yaml" \
             --config-patch @patch/cp/vip.yaml \
+            --config-patch @patch/cp/etcd-metrics-patch.yaml \
             --config-patch @/tmp/labels.yaml
     done
 
