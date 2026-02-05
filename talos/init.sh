6eef8dfc20d2f62d008281258fd82cffb795c366
Sebastian Karlsen <sebastian@karlsen.fr>
2026-02-05 22:03:49 +0100

    Disable mertrics-server: use helm chart instead with --kubelet-insecure-tls

--- talos/init.sh
+++ talos/init.sh
@@ -13,8 +13,7 @@ main()
     talosctl gen config "$CLUSTER_NAME" "https://$CONTROL_PLANE_IP:6443" --output-dir "$TALOS_CONFIG_HOME" --install-image "$FACTORY_IMAGE" \
         --config-patch @patch/common/diskSelector.yaml \
         --config-patch @patch/common/longhorn.yaml \
-        --config-patch @patch/common/cni-proxy.yaml \
-        --config-patch @patch/common/metrics-server.yaml
+        --config-patch @patch/common/cni-proxy.yaml
 
     ln -sf "$CLUSTER_NAME/talosconfig" "$XDG_CONFIG_HOME/talos/config.yaml"
 
