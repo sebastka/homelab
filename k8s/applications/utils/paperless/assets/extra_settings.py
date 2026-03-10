0dce1796dc157fbc4c5dfedea127af3641bb7fa5
Sebastian Karlsen <sebastian@karlsen.fr>
2026-03-10 13:08:34 +0100

    Div improvements

---
+++ k8s/applications/utils/paperless/assets/extra_settings.py
@@ -0,0 +1,11 @@
+
+##########################
+# Extra Django settings
+# https://docs.djangoproject.com/en/6.0/ref/settings/
+##########################
+
+CSRF_COOKIE_DOMAIN: Final[str] = os.getenv("PAPERLESS_CUSTOM_COOKIE_DOMAIN", None)
+CSRF_COOKIE_HTTPONLY: Final[bool] = __get_boolean("PAPERLESS_CUSTOM_COOKIE_HTTPONLY", "yes")
+CSRF_COOKIE_PATH: Final[str] = os.getenv("PAPERLESS_CUSTOM_COOKIE_PATH", "/")
+CSRF_COOKIE_SAMESITE: Final[str] = os.getenv("PAPERLESS_CUSTOM_COOKIE_SAMESITE", "Strict")
+CSRF_COOKIE_SECURE: Final[bool] = __get_boolean("PAPERLESS_CUSTOM_COOKIE_SECURE", "yes")
