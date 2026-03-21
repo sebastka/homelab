beee3840344b6ab568921bd18dcbe99e13372c61
Sebastian Karlsen <sebastian@karlsen.fr>
2026-03-21 13:31:26 +0100

    Remove turn and matrix dns srv records

--- terraform/dns/records_karlsenfr.tf
+++ terraform/dns/records_karlsenfr.tf
@@ -103,100 +103,100 @@ resource "cloudflare_dns_record" "karlsenfr_txt_spf" {
   zone_id = cloudflare_zone.karlsenfr.id
 }
 
-resource "cloudflare_dns_record" "karlsenfr_srv_tcp_turn" {
-  name    = "_turn._tcp"
-  proxied = false
-  ttl     = 1
-  type    = "SRV"
-  zone_id = cloudflare_zone.karlsenfr.id
+# resource "cloudflare_dns_record" "karlsenfr_srv_tcp_turn" {
+#   name    = "_turn._tcp"
+#   proxied = false
+#   ttl     = 1
+#   type    = "SRV"
+#   zone_id = cloudflare_zone.karlsenfr.id
 
-  priority = 10
-  data = {
-    service  = "_turn"
-    proto    = "_tcp"
-    name     = cloudflare_zone.karlsenfr.name
-    priority = 10
-    weight   = 5
-    port     = 5349
-    target   = "turn.karlsen.fr"
-  }
-}
+#   priority = 10
+#   data = {
+#     service  = "_turn"
+#     proto    = "_tcp"
+#     name     = cloudflare_zone.karlsenfr.name
+#     priority = 10
+#     weight   = 5
+#     port     = 5349
+#     target   = "turn.karlsen.fr"
+#   }
+# }
 
-resource "cloudflare_dns_record" "karlsenfr_srv_udp_turn" {
-  name    = "_turn._udp"
-  proxied = false
-  ttl     = 1
-  type    = "SRV"
-  zone_id = cloudflare_zone.karlsenfr.id
+# resource "cloudflare_dns_record" "karlsenfr_srv_udp_turn" {
+#   name    = "_turn._udp"
+#   proxied = false
+#   ttl     = 1
+#   type    = "SRV"
+#   zone_id = cloudflare_zone.karlsenfr.id
 
-  priority = 10
-  data = {
-    service  = "_turn"
-    proto    = "_udp"
-    name     = cloudflare_zone.karlsenfr.name
-    priority = 10
-    weight   = 5
-    port     = 3478
-    target   = "turn.karlsen.fr"
-  }
-}
+#   priority = 10
+#   data = {
+#     service  = "_turn"
+#     proto    = "_udp"
+#     name     = cloudflare_zone.karlsenfr.name
+#     priority = 10
+#     weight   = 5
+#     port     = 3478
+#     target   = "turn.karlsen.fr"
+#   }
+# }
 
-resource "cloudflare_dns_record" "karlsenfr_srv_tcp_stun" {
-  name    = "_stun._tcp"
-  proxied = false
-  ttl     = 1
-  type    = "SRV"
-  zone_id = cloudflare_zone.karlsenfr.id
+# resource "cloudflare_dns_record" "karlsenfr_srv_tcp_stun" {
+#   name    = "_stun._tcp"
+#   proxied = false
+#   ttl     = 1
+#   type    = "SRV"
+#   zone_id = cloudflare_zone.karlsenfr.id
 
-  priority = 10
-  data = {
-    service  = "_stun"
-    proto    = "_tcp"
-    name     = cloudflare_zone.karlsenfr.name
-    priority = 10
-    weight   = 5
-    port     = 3478
-    target   = "turn.karlsen.fr"
-  }
-}
+#   priority = 10
+#   data = {
+#     service  = "_stun"
+#     proto    = "_tcp"
+#     name     = cloudflare_zone.karlsenfr.name
+#     priority = 10
+#     weight   = 5
+#     port     = 3478
+#     target   = "turn.karlsen.fr"
+#   }
+# }
 
-resource "cloudflare_dns_record" "karlsenfr_srv_udp_stun" {
-  name    = "_stun._udp"
-  proxied = false
-  ttl     = 1
-  type    = "SRV"
-  zone_id = cloudflare_zone.karlsenfr.id
+# resource "cloudflare_dns_record" "karlsenfr_srv_udp_stun" {
+#   name    = "_stun._udp"
+#   proxied = false
+#   ttl     = 1
+#   type    = "SRV"
+#   zone_id = cloudflare_zone.karlsenfr.id
 
-  priority = 10
-  data = {
-    service  = "_stun"
-    proto    = "_udp"
-    name     = cloudflare_zone.karlsenfr.name
-    priority = 10
-    weight   = 5
-    port     = 3478
-    target   = "turn.karlsen.fr"
-  }
-}
+#   priority = 10
+#   data = {
+#     service  = "_stun"
+#     proto    = "_udp"
+#     name     = cloudflare_zone.karlsenfr.name
+#     priority = 10
+#     weight   = 5
+#     port     = 3478
+#     target   = "turn.karlsen.fr"
+#   }
+# }
 
-resource "cloudflare_dns_record" "karlsenfr_srv_tcp_matrix" {
-  name    = "_matrix._tcp"
-  proxied = false
-  ttl     = 1
-  type    = "SRV"
-  zone_id = cloudflare_zone.karlsenfr.id
+# resource "cloudflare_dns_record" "karlsenfr_srv_tcp_matrix" {
+#   name    = "_matrix._tcp"
+#   proxied = false
+#   ttl     = 1
+#   type    = "SRV"
+#   zone_id = cloudflare_zone.karlsenfr.id
 
-  priority = 10
-  data = {
-    service  = "_matrix"
-    proto    = "_tcp"
-    name     = cloudflare_zone.karlsenfr.name
-    priority = 10
-    weight   = 1
-    port     = 443
-    target   = "matrix.karlsen.fr"
-  }
-}
+#   priority = 10
+#   data = {
+#     service  = "_matrix"
+#     proto    = "_tcp"
+#     name     = cloudflare_zone.karlsenfr.name
+#     priority = 10
+#     weight   = 1
+#     port     = 443
+#     target   = "matrix.karlsen.fr"
+#   }
+# }
 
 resource "cloudflare_dns_record" "karlsenfr_openpgpkey_sebastian" {
   content = "mDMEYZFg5xYJKwYBBAHaRw8BAQdARkJQu8J21Vqh0r4PYqMvEAlvS0xV9o6+Lh2EVirdkCq0MlNlYmFzdGlhbiBLYXJsc2VuIChQcml2YXRlKSA8c2ViYXN0aWFuQGthcmxzZW4uZnI+iJAEExYIADgWIQTTU1/5jMz2k5rpTjOUhjx/mG1l6AUCYZFg5wIbAQULCQgHAgYVCgkICwIEFgIDAQIeAQIXgAAKCRCUhjx/mG1l6H6IAQDPdpRW/IEM5DULdVyJVME5sVwHiSE7n6CGti0Yh8rISQD6A4PbGQEKWycExd7yQflUb7ZxORMGI9QpRjNXtHx9HQK0MVNlYmFzdGlhbiBLYXJsc2VuIChXb3JrKSA8c2ViYXN0aWFuQGZqb3JkbWFpbC5ubz6InQQwFgoARRYhBNNTX/mMzPaTmulOM5SGPH+YbWXoBQJnXsPWJx0gV29yayBkb21haW4gY2hhbmdlZCB0byBjb3JwLmluYm94LmNvbQAKCRCUhjx/mG1l6LR7AQC2HsdyrIkplszK7o0H24lhxbkHBmQb25r+Jjy/5M8X1QD/TmAHbPVOL/txk+D5pWjZnxK+ika3LogLADYMxfVqcw60NFNlYmFzdGlhbiBLYXJsc2VuIChVbml2ZXJzaXR5KSA8c2ViYXNrYXJAaWZpLnVpby5ubz6InQQwFgoARRYhBNNTX/mMzPaTmulOM5SGPH+YbWXoBQJnXsIVJx0gRS1tYWlsIGFkZHJlc3MgZG9lcyBub3QgZXhpc3QgYW55bW9yZQAKCRCUhjx/mG1l6LD9AQC0NY2CfWJH52Ele9otU8QY3GJO9GJNeE00UJ21tonG6gEA9s1SW3PA9AgwvObJQeeYDOZfNMFmO3Mb6ba9T7Gynw60M1NlYmFzdGlhbiBLYXJsc2VuIChXb3JrKSA8c2ViYXN0aWFuQGNvcnAuaW5ib3guY29tPoiTBBMWCgA7FiEE01Nf+YzM9pOa6U4zlIY8f5htZegFAmdexCMCGwEFCwkIBwICIgIGFQoJCAsCBBYCAwECHgcCF4AACgkQlIY8f5htZehzlQEAks1pvAhD6BFZIFKwzFL/iUmcX6b2uQN2xlMbZpAJEhQA/ReXlm6tbS1WZ4tLw1EV1Mg2rcWE6F6y3GTdK7yqX4oIuDMEYZFhZxYJKwYBBAHaRw8BAQdAsqqAjdHbHdyD6NiYZ1meMXChG74sCm0GAaKjrHlSn1SI7wQYFggAIBYhBNNTX/mMzPaTmulOM5SGPH+YbWXoBQJhkWFnAhsCAIEJEJSGPH+YbWXodiAEGRYIAB0WIQTphQ9cga/mRWIKe8qkxouyvIh8ugUCYZFhZwAKCRCkxouyvIh8upR/AP4khgoCLl7A1sXvfIH3uM8sqTWKeDVbPRHohFVuWNpSRwEAksyV8EODh0fv4c6esTBT6BuqozCwk39sBizAOiRzqgLNJQEA2ZxSGhjcu//hznswl7z5IHXR1DOz9AHnDFoMs5FJp4AA/iR3Agx7iQZhH7ob1RqX1uwa76cJfLbBwdvEzcEQUFsDuDgEYZFhlBIKKwYBBAGXVQEFAQEHQHa6NZ1swrLC2f3rJjQ5CZq+Sy358l1S1nVuqd35iFscAwEIB4h4BBgWCAAgFiEE01Nf+YzM9pOa6U4zlIY8f5htZegFAmGRYZQCGwwACgkQlIY8f5htZegc7QD9HJ3++mbnkOv4QQ5Xfl64n8gI/NInu2subaESMVUqRWIA/05ZDle1Iquts/ts/m5qArHXCO7/Upum6IxF7N8dFpAKuDMEYZFhqRYJKwYBBAHaRw8BAQdA8puk36/6BIWGPWYmP1y/7jri6YpLOVK9wQ9ZpALg7XqIeAQYFggAIBYhBNNTX/mMzPaTmulOM5SGPH+YbWXoBQJhkWGpAhsgAAoJEJSGPH+YbWXoXggBAAC6TeH1LdTUCnym+w3G81Npyh1cAAISlpNMfBRQsqvyAP9+SIh+h2hKsSror0xPfiO+2AGl670MgoTHD8Vp2kLxBQ=="
