39e56a11ea00ac6d8ad9bc5031c7818e2117b1e4
Sebastian Karlsen <sebastian@karlsen.fr>
2026-03-28 14:43:58 +0100

    Update init script and patches

--- talos/init.sh
+++ talos/init.sh
@@ -7,21 +7,15 @@ main()
     export CLUSTER_NAME="$1"
     [ -f "./clusters/${CLUSTER_NAME}/cluster.env" ] || return 1
     set -a; . "./clusters/${CLUSTER_NAME}/cluster.env"; set +a
+    
+    talos_generate
+    talos_apply
+    talos_bootstrap
+    talos_kubeconfig
+}
 
-    # Hacky installation of Cilium during cluster bootstrap
-    # yq -n \
-    #     '.cluster.inlineManifests[0].name = "cilium-install" |
-    #     .cluster.inlineManifests[0].contents = loadstr("/tmp/cilium-helm.yaml")' \
-    #     >/tmp/cilium-install.yaml
-    # printf -- '---\ncluster:\n  inlineManifests:\n    - name: cilium\n      contents: |\n' \
-    #     >/tmp/cilium-install.yaml
-    # helm template cilium oci://quay.io/cilium/charts/cilium \
-    #     --version 1.19.1 \
-    #     --namespace kube-system \
-    #     -f ../k8s/infrastructure/network/cilium/values.yaml \
-    #     | sed 's/^/        /' \
-    #     >>/tmp/cilium-install.yaml
-
+talos_generate()
+{
     # Extracting the Sealed Secrets controller certificate and key from the SOPS encrypted secret file
     export SEALED_SECRETS_CRT="$(sops -d "./clusters/${CLUSTER_NAME}/talsecret.sops.enc.yaml" | yq -r '.certs.sealedsecrets.crt')"
     export SEALED_SECRETS_KEY="$(sops -d "./clusters/${CLUSTER_NAME}/talsecret.sops.enc.yaml" | yq -r '.certs.sealedsecrets.key')"
@@ -33,28 +27,54 @@ main()
         --secret-file "./clusters/${CLUSTER_NAME}/talsecret.sops.enc.yaml" \
         --no-gitignore \
         --out-dir "$TALOS_CONFIG_HOME"
-    ln -sf "$CLUSTER_NAME/talosconfig" "$XDG_CONFIG_HOME/talos/config.yaml"
+    mv "$TALOS_CONFIG_HOME/talosconfig" "$TALOS_CONFIG_HOME/config.yaml"
+    ln -sf "$CLUSTER_NAME/config.yaml" "$XDG_CONFIG_HOME/talos/config.yaml"
 
-    wait 'Press enter to apply configuration to control plane nodes...'
-    sed 1d "./clusters/${CLUSTER_NAME}/nodes.csv" | grep -E -v '^#' | while IFS=, read cluster pve_node node_name hw_add net_addr; do
-        talosctl apply-config --insecure --talosconfig="$TALOS_CONFIG_HOME/talosconfig" --nodes "${net_addr}" --file "$TALOS_CONFIG_HOME/talmox-${node_name}.talmox.hera.home.karlsen.fr.yaml"
-    done
-
-    wait 'Press enter to bootstrap the cluster...'
-    talosctl bootstrap --talosconfig="$TALOS_CONFIG_HOME/talosconfig" --nodes "${CONTROL_PLANE_IP}"
-    printf 'Bootstrapping the cluster, this may take a few minutes...\n'
-    sleep 30
-
-    wait 'Press enter to retrieve the kubeconfig file...'
-    [ ! -f "$XDG_CONFIG_HOME/kube/config.$CLUSTER_NAME" ] || rm "$XDG_CONFIG_HOME/kube/config.$CLUSTER_NAME"
-    talosctl kubeconfig "$XDG_CONFIG_HOME/kube/config.$CLUSTER_NAME" --nodes "$CONTROL_PLANE_IP"
-    ln -sf "config.$CLUSTER_NAME" "$XDG_CONFIG_HOME/kube/config"
+    # Format and only use first endpoint and node
+    yq eval-all -i '
+        . head_comment |= "Endpoints: " + (.contexts[env(CLUSTER_NAME)].endpoints| join(", ")) + "\nNodes: " + (.contexts[env(CLUSTER_NAME)].nodes| join(", ")) |
+        .contexts[env(CLUSTER_NAME)].endpoints = [.contexts[env(CLUSTER_NAME)].endpoints[0]] |
+        .contexts[env(CLUSTER_NAME)].nodes = [.contexts[env(CLUSTER_NAME)].nodes[0]]
+        ' "$TALOS_CONFIG_HOME/config.yaml"
 }
 
-wait()
+talos_apply()
 {
-    printf -- "$1"
-    read a
+    printf -- 'Press enter to apply configuration to nodes...'
+    read keypress
+
+    sed 1d "./clusters/${CLUSTER_NAME}/nodes.csv" | grep -E -v '^#' | while IFS=, read cluster pve_node node_name hw_add net_addr; do
+        talosctl apply-config --insecure \
+            --nodes "${net_addr}" \
+            --talosconfig "$TALOS_CONFIG_HOME/config.yaml" \
+            --file "$TALOS_CONFIG_HOME/talmox-${node_name}.talmox.hera.home.karlsen.fr.yaml"
+    done
+}
+
+talos_bootstrap()
+{
+    printf -- 'Press enter to bootstrap the cluster...'
+    read keypress
+
+    talosctl bootstrap --nodes "${CONTROL_PLANE_IP}" \
+        --talosconfig "$TALOS_CONFIG_HOME/config.yaml"
+    printf 'Bootstrapping the cluster, this may take a few minutes...\n'
+    sleep 30
+}
+
+talos_kubeconfig()
+{
+    printf -- 'Press enter to retrieve the kubeconfig file...'
+    read keypress
+
+    [ ! -d "$XDG_CONFIG_HOME/kube/$CLUSTER_NAME" ] || rm -rf "$XDG_CONFIG_HOME/kube/$CLUSTER_NAME"
+    mkdir -p "$XDG_CONFIG_HOME/kube/$CLUSTER_NAME"
+
+    talosctl kubeconfig "$XDG_CONFIG_HOME/kube/$CLUSTER_NAME/config.yaml" \
+        --nodes "$CONTROL_PLANE_IP" \
+        --talosconfig "$TALOS_CONFIG_HOME/config.yaml"
+
+    ln -sf "$CLUSTER_NAME/config.yaml" "$XDG_CONFIG_HOME/kube/config"
 }
 
 main "$@"
