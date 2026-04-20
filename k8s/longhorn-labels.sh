2c55e6d29c2897c9ee9bb0090f3a56028a0ba0aa
Sebastian Karlsen <sebastian@karlsen.fr>
2026-04-21 00:21:26 +0200

    fix: Update labelling script

--- k8s/longhorn-labels.sh
+++ k8s/longhorn-labels.sh
@@ -2,10 +2,15 @@
 set -eux
 
 for vol in \
-    longhorn-paperless-data-pv longhorn-paperless-export-pv longhorn-paperless-consume-pv longhorn-paperless-media-pv \
-    deluge-config-pv \
+    deluged-config-pv \
+    forgejo-shared-storage-pv \
+    harbor-joblog-pv harbor-redis-pv harbor-registry-pv harbor-trivy-pv \
     jellyfin-config-pv \
-    longhorn-portainer-pv \
+    metube-persistent-pv \
+    paperless-data-pv paperless-media-pv \
+    portainer-data-pv \
+    slink-persist-pv \
+    synapse-data-pv \
     thelounge-data-pv
 do
     kubectl -n longhorn-system label "volume/${vol}" recurring-job-group.longhorn.io/group1=enabled
