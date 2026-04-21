3d613bd47c02f6bd573e650c8db63b4f15684537
Sebastian Karlsen <sebastian@karlsen.fr>
2026-04-21 18:47:45 +0200

    feat: Update labels

--- k8s/longhorn-labels.sh
+++ k8s/longhorn-labels.sh
@@ -1,18 +1,40 @@
 #!/bin/sh
 set -eux
 
+# Group1: Nightly full backup with 7 day retention
 for vol in \
     deluged-config-pv \
+    paperless-data-pv paperless-media-pv \
+    synapse-data-pv
+do
+    for label in \
+        'recurring-job-group.longhorn.io/default-' \
+        'recurring-job-group.longhorn.io/group1=enabled' \
+        'recurring-job-group.longhorn.io/group2-' \
+        'backup-target-' \
+        'backup-target=nfs-backup-target'
+    do
+        kubectl -n longhorn-system label "volume/${vol}" "$label"
+    done
+done
+
+# Group2: Nightly incremental backup with 7 day retention
+for vol in \
+    jellyfin-config-pv \
     forgejo-shared-storage-pv \
     harbor-joblog-pv harbor-redis-pv harbor-registry-pv harbor-trivy-pv \
-    jellyfin-config-pv \
     metube-persistent-pv \
-    paperless-data-pv paperless-media-pv \
     portainer-data-pv \
-    slink-persist-pv \
-    synapse-data-pv \
+    slink-app-pv \
     thelounge-data-pv
 do
-    kubectl -n longhorn-system label "volume/${vol}" recurring-job-group.longhorn.io/group1=enabled
-    kubectl -n longhorn-system label "volume/${vol}" recurring-job-group.longhorn.io/default-
+    for label in \
+        'recurring-job-group.longhorn.io/default-' \
+        'recurring-job-group.longhorn.io/group1-' \
+        'recurring-job-group.longhorn.io/group2=enabled' \
+        'backup-target-' \
+        'backup-target=nfs-backup-target'
+    do
+        kubectl -n longhorn-system label "volume/${vol}" "$label"
+    done
 done
