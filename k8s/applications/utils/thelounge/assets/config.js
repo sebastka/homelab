1bb8e61d454354c7891c6bb525aa64ac4d34e595
Sebastian Karlsen <sebastian@karlsen.fr>
2026-05-19 15:38:25 +0200

    feat: ldap auth

--- k8s/applications/utils/thelounge/assets/config.js
+++ k8s/applications/utils/thelounge/assets/config.js
@@ -379,89 +379,31 @@ module.exports = {
         //
         // They are only being used in private mode. To know more about private mode,
         // see the `public` setting above.
-
-        //
-        // The authentication process works as follows:
-        //
-        // 1. The Lounge connects to the LDAP server with its system credentials.
-        // 2. It performs an LDAP search query to find the full DN associated to the
-        //    user requesting to log in.
-        // 3. The Lounge tries to connect a second time, but this time using the
-        //    user's DN and password. Authentication is validated if and only if this
-        //    connection is successful.
-        //
-        // The search query takes a couple of parameters in `searchDN`:
-        //
-        // - a base DN `searchDN/base`. Only children nodes of this DN will likely
-        //   be returned;
-        // - a search scope `searchDN/scope` (see LDAP documentation);
-        // - the query itself, built as `(&(<primaryKey>=<username>) <filter>)`
-        //   where `<username>` is the user name provided in the log in request,
-        //   `<primaryKey>` is provided by the config and `<filter>` is a filtering
-        //   complement also given in the config, to filter for instance only for
-        //   nodes of type `inetOrgPerson`, or whatever LDAP search allows.
-        //
-        // Alternatively, you can specify the `bindDN` parameter. This will make The
-        // Lounge ignore `searchDN` options and assume that the user DN is always
-        // `<bindDN>,<primaryKey>=<username>`, where `<username>` is the user name
-        // provided in the log in request, and `<bindDN>` and `<primaryKey>` are
-        // provided by the configuration.
         //
         // The available keys for the `ldap` object are:
         ldap: {
                 // - `enable`: when set to `false`, LDAP support is disabled and all other
                 //   values are ignored.
-                enable: false,
+                enable: true,
 
                 // - `url`: A url of the form `ldaps://<ip>:<port>`.
                 //   For plain connections, use the `ldap` scheme.
-                url: "ldaps://example.com",
+                url: "ldaps://light-ldap.light-ldap.svc.cluster.local:636",
 
                 // - `tlsOptions`: LDAP connection TLS options (only used if scheme is
                 //   `ldaps://`). It is an object whose values are Node.js' `tls.connect()`
                 //   options. It is set to `{}` by default.
-                //   For example, this option can be used in order to force the use of IPv6:
-                //   ```js
-                //   {
-                //     host: 'my::ip::v6',
-                //     servername: 'example.com'
-                //   }
-                //   ```
-                tlsOptions: {},
+                tlsOptions: {
+                        rejectUnauthorized: true,
+                        ca: require("fs").readFileSync("/etc/ssl/cluster-ca/cluster-root-ca.crt"),
+                },
 
                 // - `primaryKey`: LDAP primary key. It is set to `"uid"` by default.
                 primaryKey: "uid",
 
-                // - `baseDN`: LDAP base DN, alternative to `searchDN`. For example, set it
-                //   to `"ou=accounts,dc=example,dc=com"`.
-                //   When unset, the LDAP auth logic with use `searchDN` instead to locate users.
-
-                // - `searchDN`: LDAP search DN settings. This defines the procedure by
-                //   which The Lounge first looks for the user DN before authenticating them.
-                //   It is ignored if `baseDN` is specified. It is an object with the
-                //   following keys:
-                searchDN: {
-                        //   - `rootDN`: This bind DN is used to query the server for the DN of
-                        //     the user. This is supposed to be a system user that has access in
-                        //     read-only to the DNs of the people that are allowed to log in.
-                        //     It is set to `"cn=thelounge,ou=system-users,dc=example,dc=com"` by
-                        //     default.
-                        rootDN: "cn=thelounge,ou=system-users,dc=example,dc=com",
-
-                        //   - `rootPassword`: Password of The Lounge LDAP system user.
-                        rootPassword: "1234",
-
-                        //   - `ldapFilter`: it is set to `"(objectClass=person)(memberOf=ou=accounts,dc=example,dc=com)"`
-                        //     by default.
-                        filter: "(objectClass=person)(memberOf=ou=accounts,dc=example,dc=com)",
-
-                        //   - `base`: LDAP search base (search only within this node). It is set
-                        //     to `"dc=example,dc=com"` by default.
-                        base: "dc=example,dc=com",
-
-                        //   - `scope`: LDAP search scope. It is set to `"sub"` by default.
-                        scope: "sub",
-                },
+                // - `baseDN`: LDAP base DN. TheLounge binds directly as
+                //   `uid=<username>,ou=people,dc=karlsen,dc=fr` — no service account needed.
+                baseDN: "ou=people,dc=karlsen,dc=fr",
         },
 
         // ## Debugging settings
